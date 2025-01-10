f69c87bf491400807ea58b4820ff4f93aa8bae34
richieArnold <arnoldrichie31.rar@gmail.com>
2025-01-10 10:17:01 +0530

    create project and add images

---
+++ app/(tabs)/index.jsx
@@ -0,0 +1,41 @@
+// to add styles import StyleSheet
+import { View, Text, StyleSheet, ImageBackground } from "react-native";
+import icedCoffeeImg from "@/assets/images/iced-coffee.png";
+import React from "react";
+
+const app = () => {
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        source={icedCoffeeImg}
+        resizeMode="cover"
+        style={styles.image}
+      >
+        <Text style={styles.text}>Coffee Shop</Text>
+      </ImageBackground>
+    </View>
+  );
+};
+
+export default app;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 42,
+    fontWeight: "bold",
+    textAlign: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    color: "white",
+  },
+});
